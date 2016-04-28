820a4efe2bd8ab2167dfb56f5a9915675a37f9e8
ispedals <ispedals@users.noreply.github.com>
2016-04-28 17:20:04 -0400

    Show menu even if media is paused (#4)
    We used to only show the menu if there was a single un-paused media
    element. We did this because it was easier to identify which element
    should be manipulated. We now first check to see if there is only one
    media element anyway and just act on that. If there is more than one, we
    use not being paused as the criterion to find the element. Bump version
    and update build

--- data/manipulator.js
+++ data/manipulator.js
@@ -1,16 +1,16 @@
-self.on("context", function (node) {
-  let elems = Array.prototype.slice.call(document.querySelectorAll("video, audio"));
-  /*
-    TODO
-    Content scripts can't access content hosted in an iframe, if that content is served from a different domain,
-    so we can't access videos in iframes, which is a problem with iframe embeded videos
-    https://addons.mozilla.org/en-US/developers/docs/sdk/latest/dev-guide/guides/content-scripts/cross-domain.html
+function getElement(node){
+	if(/^(video|audio)$/i.test(node.tagName)){
+		return [node];
+	}
+	let elems = Array.prototype.slice.call(document.querySelectorAll("video, audio"));
+	if(elems.length <= 1){
+		return elems;
+	}
+	return elems.filter(elem => !elem.paused);
+}
 
-    we could try whitelisting domains under the "cross-domain-content" key for various video sites
-  */
-  elems = elems.filter(function (elem) {
-    return !elem.paused;
-  });
+self.on("context", function (node) {
+  let elems = getElement(node);
   if (elems.length === 1) {
     self.postMessage(elems[0].playbackRate + "X");
     return true;
@@ -21,10 +21,10 @@ self.on("context", function (node) {
 });
 
 self.on("click", function (node, data) {
-  let elems = Array.prototype.slice.call(document.querySelectorAll("video, audio"));
-  elems = elems.filter(function (elem) {
-    return !elem.paused;
-  });
+  let elems = getElement(node);
+  if(elems.length !== 1){
+	  return;
+  }
   if (data.endsWith('X')) {
     //fixed value, strip X
     let rate = data.slice(0, -1);
